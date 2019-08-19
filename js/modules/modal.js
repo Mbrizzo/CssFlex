f0c2daecb2499fc76ffcbe42b64120e5b5ae88d4
Mbrizzo <mrizzo25@gmail.com>
2019-08-18 23:43:56 -0300

    JS-Modal Add

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,6 +1,21 @@
 export default function initModal() {
 
   console.log('opa');
+  
+  // const btnAbrir = document.querySelector('[data-modal="abrir"]');
+  // const btnFechar = document.querySelector('[data-modal="fechar"]');
+  // const containerModal = document.querySelector('[data-modal="container"]');
+
+  // function abrirModal() {
+
+  // }
+
+  // function fecharModal(){
+
+  // }
+
+  // btnAbrir.addEventListener('click', abrirModal);
+  // btnFechar.addEventListener('click', fecharModal);
 
 }
 
