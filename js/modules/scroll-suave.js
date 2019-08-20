99caaf46f57250d68a4f3a76b928da603fd89ca9
Mbrizzo <mrizzo25@gmail.com>
2019-08-20 11:27:20 -0300

    erro no scroll (em construção)

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,7 +1,14 @@
 export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+  const linksInternos = document.querySelectorAll('[data-menu="suave" a[href^="#"]');
 }
 
-function scroll() {
-  
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  console.log(href);
 }
+
+linksInternos.array.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+
